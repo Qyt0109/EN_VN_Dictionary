13c5706ba857cdd063a22ccef9b26397cc2fc990
DaoQuyet <nmhtdyt@gmail.com>
2023-04-09 02:40:17 +0700

    no message

---
+++ Code/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EN_VN_Lib
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            GlobalVariable.database.loadDB();
+            GlobalVariable.database.loadSettings();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new DictionaryForm());
+        }
+    }
+}
